ab731723b1bcf5f4dfc3fae6423ac290ccd19c39
APPLE4869 <apple.4869.s@gmail.com>
2018-08-27 18:19:39 +0900

    f

--- partials/javascript.php
+++ partials/javascript.php
@@ -1,6 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-  $((){
+  (function() {
     //全てのaタグについて処理
     $('a').each(function(){
       //aタグのhref属性からリンク先url取得
