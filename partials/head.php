3bdacb2560278eb859aa2529aef4c757b68859a8
APPLE4869 <apple.4869.s@gmail.com>
2018-08-27 18:27:51 +0900

    f

--- partials/head.php
+++ partials/head.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <!--ホームアイコンの設定-->
 <link rel="apple-touch-icon" href="partials/apple-touch-icon.png" />
