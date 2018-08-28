cc904996da5c9ddefaa3a04348961e9d9a7240b0
APPLE4869 <apple.4869.s@gmail.com>
2018-08-28 18:14:17 +0900

    f

--- partials/head.php
+++ partials/head.php
@@ -1,6 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 
-<link rel="manifest" href="manifest.json">
+<link rel="manifest" href="./manifest.json">
 
 <!-- IOS用のホームアイコン -->
 <link rel="apple-touch-startup-image" href="images/icons/icon-144x144.png">
@@ -8,7 +8,7 @@
 <meta name="apple-mobile-web-app-capable" content="yes" />
 
 <!--ホームアイコンの設定-->
-<link rel="apple-touch-icon" href="partials/apple-touch-icon.png" />
+<link rel="apple-touch-icon" href="images/icons/icon-144x144.png" />
 
 <!-- テーマカラー -->
 <meta name="theme-color" content="#005500">
