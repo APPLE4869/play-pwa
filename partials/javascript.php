f3b161182e401dc66125fa3fd395701e9dd3733f
APPLE4869 <apple.4869.s@gmail.com>
2018-08-28 14:34:59 +0900

    f

--- partials/javascript.php
+++ partials/javascript.php
@@ -19,7 +19,7 @@
   })();
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./partials/sw.js').then(function(registration) {
       // 登録成功
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function(err) {
