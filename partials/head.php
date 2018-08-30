c0bca2e1870596a9c868f2bc0b1f210a818038f7
APPLE4869 <apple.4869.s@gmail.com>
2018-08-30 16:46:33 +0900

    f

--- partials/head.php
+++ partials/head.php
@@ -23,24 +23,13 @@
 
 <!-- ここからスプラッシュ画像の設定 -->
 
-<!-- iPhone 6 Plus portrait startup image -->
-<link href="images/screen/Default~iphone.png"
-      media="(device-width: 414px) and (device-height: 736px)
-             and (-webkit-device-pixel-ratio: 3)
-             and (orientation: portrait)"
-      rel="apple-touch-startup-image">
-
-<!-- iPhone 6 startup image -->
-<link href="images/screen/Default~iphone.png"
-      media="(device-width: 375px) and (device-height: 667px)
-             and (-webkit-device-pixel-ratio: 2)"
-      rel="apple-touch-startup-image">
-
-<!-- iPhone 5 startup image -->
-<link href="images/screen/Default~iphone.png"
-      media="(device-width: 320px) and (device-height: 568px)
-             and (-webkit-device-pixel-ratio: 2)"
-      rel="apple-touch-startup-image">
+<link href="images/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+<link href="images/splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
 
 <!-- ここまでスプラッシュ画像の設定 -->
 
