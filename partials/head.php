6b08ab53a2a17bf48a098a4562f4ca3fd321cf62
APPLE4869 <apple.4869.s@gmail.com>
2018-08-30 16:34:27 +0900

    f

--- partials/head.php
+++ partials/head.php
@@ -1,19 +1,94 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 
 <link rel="manifest" href="./manifest.json">
+<link rel="stylesheet" href="./partials/app.css">
 
-<!-- IOS用のホームアイコン -->
-<link rel="apple-touch-startup-image" href="images/icons/icon-144x144.png">
 
+<!-- ここからIOS用のPWA設定 -->
+
+<!-- ホーム画面に追加したWebアプリをネイティブアプリlikeで表示 -->
 <meta name="apple-mobile-web-app-capable" content="yes" />
 
-<!--ホームアイコンの設定-->
+<!-- ホーム画面で表示されるAPPのタイトル -->
+<meta name="apple-mobile-web-app-title" content="Appscope">
+
+<!--ホームアイコン-->
 <link rel="apple-touch-icon" href="images/icons/icon-144x144.png" />
 
 <!-- テーマカラー -->
-<meta name="theme-color" content="#005500">
+<meta name="theme-color" content="#00aa00">
 
-<!-- バーの色を変える設定 -->
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<!-- 上部のバーの色 -->
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
-<link rel="stylesheet" href="./partials/app.css">
+<!-- ここからスプラッシュ画像の設定 -->
+
+<!-- iPad retina portrait startup image -->
+<link href="https://placehold.it/1536x2008"
+      media="(device-width: 768px) and (device-height: 1024px)
+             and (-webkit-device-pixel-ratio: 2)
+             and (orientation: portrait)"
+      rel="apple-touch-startup-image">
+
+<!-- iPad retina landscape startup image -->
+<link href="https://placehold.it/1496x2048"
+      media="(device-width: 768px) and (device-height: 1024px)
+             and (-webkit-device-pixel-ratio: 2)
+             and (orientation: landscape)"
+      rel="apple-touch-startup-image">
+
+<!-- iPad non-retina portrait startup image -->
+<link href="https://placehold.it/768x1004"
+      media="(device-width: 768px) and (device-height: 1024px)
+             and (-webkit-device-pixel-ratio: 1)
+             and (orientation: portrait)"
+      rel="apple-touch-startup-image">
+
+<!-- iPad non-retina landscape startup image -->
+<link href="https://placehold.it/748x1024"
+      media="(device-width: 768px) and (device-height: 1024px)
+             and (-webkit-device-pixel-ratio: 1)
+             and (orientation: landscape)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone 6 Plus portrait startup image -->
+<link href="https://placehold.it/1242x2148"
+      media="(device-width: 414px) and (device-height: 736px)
+             and (-webkit-device-pixel-ratio: 3)
+             and (orientation: portrait)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone 6 Plus landscape startup image -->
+<link href="https://placehold.it/1182x2208"
+      media="(device-width: 414px) and (device-height: 736px)
+             and (-webkit-device-pixel-ratio: 3)
+             and (orientation: landscape)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone 6 startup image -->
+<link href="https://placehold.it/750x1294"
+      media="(device-width: 375px) and (device-height: 667px)
+             and (-webkit-device-pixel-ratio: 2)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone 5 startup image -->
+<link href="https://placehold.it/640x1096"
+      media="(device-width: 320px) and (device-height: 568px)
+             and (-webkit-device-pixel-ratio: 2)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone < 5 retina startup image -->
+<link href="https://placehold.it/640x920"
+      media="(device-width: 320px) and (device-height: 480px)
+             and (-webkit-device-pixel-ratio: 2)"
+      rel="apple-touch-startup-image">
+
+<!-- iPhone < 5 non-retina startup image -->
+<link href="https://placehold.it/320x460"
+      media="(device-width: 320px) and (device-height: 480px)
+             and (-webkit-device-pixel-ratio: 1)"
+      rel="apple-touch-startup-image">
+
+<!-- ここまでスプラッシュ画像の設定 -->
+
+<!-- ここまでIOS用のPWA設定 -->
